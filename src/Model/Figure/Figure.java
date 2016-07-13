b973e65250d68a00f2af0edda372ee298526df4f
Oreshin Pavel <oreshinpe@gmail.com>
2016-07-13 13:02:49 +0300

    init

---
+++ src/Model/Figure/Figure.java
@@ -0,0 +1,9 @@
+package Model.Figure;
+
+import Model.Position;;
+
+public abstract class Figure {
+	
+	private Position position;	
+	
+}
---
+++ src/Model/Position.java
@@ -0,0 +1,9 @@
+package Model;
+
+public class Position {
+	
+	int row;
+	
+	int column;
+
+}
