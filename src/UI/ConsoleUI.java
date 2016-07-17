96aef98f7cb2c3ac5bd44d334e0e944011adff66
Oreshin Pavel <oreshinpe@gmail.com>
2016-07-17 23:08:23 +0300

    board representation fix

--- src/UI/ConsoleUI.java
+++ src/UI/ConsoleUI.java
@@ -12,6 +12,16 @@ import Game.Figure.*;
 
 public class ConsoleUI {
 	
+	public static final String ANSI_RESET = "\u001B[0m";
+	public static final String ANSI_BLACK = "\u001B[30m";
+	public static final String ANSI_RED = "\u001B[31m";
+	public static final String ANSI_GREEN = "\u001B[32m";
+	public static final String ANSI_YELLOW = "\u001B[33m";
+	public static final String ANSI_BLUE = "\u001B[34m";
+	public static final String ANSI_PURPLE = "\u001B[35m";
+	public static final String ANSI_CYAN = "\u001B[36m";
+	public static final String ANSI_WHITE = "\u001B[37m";
+	
 	private static ConsoleUI instance = null;
 	
 	public static ConsoleUI getInstance()
@@ -29,8 +39,6 @@ public class ConsoleUI {
 		
 		String s = sc.next();
 		
-		sc.close();
-		
 		if (s == "W") return Colour.WHITE;
 		else return Colour.BLACK;
 	}
@@ -40,7 +48,7 @@ public class ConsoleUI {
 		Desk desk = GameController.getInstance().getDesk();
 		char [] line={'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'};
 		for (int i=0;i<=7;i++){
-			System.out.print(line[i]+" \t");
+			System.out.print((i+1) + " \t");
 				
 			for(int j=0;j<=7;j++){
 				Position pos = new Position(i, j);
@@ -57,11 +65,17 @@ public class ConsoleUI {
 			System.out.println();
 		}	
 		
+		System.out.println();
 		System.out.print("\t");
-		for(int i=1;i<=8;i++){
-			System.out.print(i);
+		for(int i=0;i<=7;i++){
+			System.out.print(line[i]);
 		}
 		System.out.println();
+	}
+	
+	public void colorOut(String text, String color)
+	{
+		System.out.println(color + text + ANSI_RESET);
 	}
 
 	
@@ -85,7 +99,6 @@ public class ConsoleUI {
 		
 		Move newMove=new Move(x1,y1,x2,y2);
 		
-		in.close();
 		return newMove;
 	}
 }
