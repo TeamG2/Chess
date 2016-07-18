e6b6bd2054687dc6eae5b78c4b486a843fef9db2
Maksim Filipenko <filipenko.artweb@gmail.com>
2016-07-18 12:44:36 +0300

    Pawn setofposiblemoves

--- src/Game/Figure/Pawn.java
+++ src/Game/Figure/Pawn.java
@@ -19,21 +19,19 @@ public class Pawn extends Figure {
 	public HashSet<Position> getPossiblePositions(Position current) {
 		// TODO Auto-generated method stub
 		HashSet<Position> setOfPosibleMoves= new HashSet <>();
-		
+		Desk desk = GameController.getInstance().getDesk();
 		if ( getColour() == Colour.WHITE){
 			int x=current.getX(),y=current.getY();
 			
 			if (y==1)System.out.print("Error! Pawn.getPossiblePositions()");
 			
-			if (y<7)setOfPosibleMoves.add(new Position(x,y+1));
+			if (y<7) if (desk.getCell(new Position(x,y+1)).isFree()) setOfPosibleMoves.add(new Position(x,y+1));
 
-			if (y==2)setOfPosibleMoves.add(new Position(x,4));
-			
-			Desk desk = GameController.getInstance().getDesk();
+			if (y==2)if (desk.getCell(new Position(x,4)).isFree()) setOfPosibleMoves.add(new Position(x,4));
 			
 			if (x>0){
 			Cell newCell = desk.getCell(new Position(x-1,y+1));
-				if  (newCell.getFigure().getColour() != this.getColour()){
+				if  (newCell.getFigure().getColour() != getColour()){
 					setOfPosibleMoves.add(new Position(x-1,y+1));
 				}
 			}
@@ -46,6 +44,30 @@ public class Pawn extends Figure {
 			}
 
 		}	
+		
+		if ( getColour() == Colour.BLACK){
+			int x=current.getX(),y=current.getY();
+			
+			if (y==7)System.out.print("Error! Pawn.getPossiblePositions()");
+			
+			if (y>0) if (desk.getCell(new Position(x,y-1)).isFree()) setOfPosibleMoves.add(new Position(x,y-1));
+
+			if (y==6)if (desk.getCell(new Position(x,5)).isFree()) setOfPosibleMoves.add(new Position(x,5));
+			
+			if (x>0){
+			Cell newCell = desk.getCell(new Position(x-1,y-1));
+				if  (newCell.getFigure().getColour() != getColour()){
+					setOfPosibleMoves.add(new Position(x-1,y-1));
+				}
+			}
+			
+			if (x<7){
+			Cell newCell = desk.getCell(new Position(x+1,y-1));
+				if  (newCell.getFigure().getColour() != this.getColour()){
+					setOfPosibleMoves.add(new Position(x+1,y+1));
+				}
+			}
+		}
 		return setOfPosibleMoves;
 	}
 }
