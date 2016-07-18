845db077d42830922e50d5934db5d272448d50cc
Maksim Filipenko <filipenko.artweb@gmail.com>
2016-07-18 21:46:32 +0300

    PAWN MOVES DONE

--- src/Game/Figure/Pawn.java
+++ src/Game/Figure/Pawn.java
@@ -19,22 +19,21 @@ public class Pawn extends Figure {
 	public HashSet<Position> getPossiblePositions(Position current) {
 		// TODO Auto-generated method stub
 		HashSet<Position> setOfPosibleMoves= new HashSet <>();
+		Desk desk = GameController.getInstance().getDesk();
 		if ( getColour() == Colour.WHITE){
 			int row = current.getRow(), column = current.getColumn();
 			
 			if (row == 0) System.out.print("Error! Pawn.getPossiblePositions()");
 			
-			if (row < 7)
+			if (row < 7 && desk.getCell(new Position(row + 1, column)).isFree())
 			{
 				setOfPosibleMoves.add(new Position(row + 1, column));
 			}
 
-			if (row == 1)
+			if (row == 1 && desk.getCell(new Position(3, column)).isFree())
 			{
 				setOfPosibleMoves.add(new Position(3, column));
 			}
-			
-			Desk desk = GameController.getInstance().getDesk();
 			
 			if (column > 0){
 				Cell newCell = desk.getCell(new Position(row + 1, column - 1));
@@ -56,29 +55,28 @@ public class Pawn extends Figure {
 			
 			if (row == 7) System.out.print("Error! Pawn.getPossiblePositions()");
 			
-			if (row > 0)
+			if (row > 0 && desk.getCell(new Position(row - 1, column)).isFree())
 			{
 				setOfPosibleMoves.add(new Position(row - 1, column));
 			}
 
-			if (row == 6)
+			if (row == 6 && desk.getCell(new Position(4, column)).isFree())
 			{
 				setOfPosibleMoves.add(new Position(4, column));
 			}
 			
-			Desk desk = GameController.getInstance().getDesk();
 			
-			if (column > 0){
+			if (column > 0 && row>0){
 				Cell newCell = desk.getCell(new Position(row - 1, column - 1));
 				if  (!newCell.isFree() && newCell.getFigure().getColour() != this.getColour()){
-					setOfPosibleMoves.add(new Position(row + 1, column - 1));
+					setOfPosibleMoves.add(new Position(row - 1, column - 1));
 				}
 			}
 			
-			if (column < 7){
+			if (column < 7 && row>0){
 				Cell newCell = desk.getCell(new Position(row - 1, column + 1));
 				if  (!newCell.isFree() && newCell.getFigure().getColour() != this.getColour()){
-					setOfPosibleMoves.add(new Position(row + 1, column + 1));
+					setOfPosibleMoves.add(new Position(row - 1, column + 1));
 				}
 			}
 		}
